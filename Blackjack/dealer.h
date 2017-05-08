80b5f0282475e4c7b9bfb7270a616038189cd76a
Ram Viswanathan <Ram@Rams-MacBook-Pro.local>
2017-05-08 04:42:17 -0700

    blackjack final impl

--- Blackjack/dealer.h
+++ Blackjack/dealer.h
@@ -1,38 +1,57 @@
 #ifndef _DEALER_H_
 #define _DEALER_H_
+
 #include <iostream>
 #include <array>
 #include <vector>
-#include <stack>
-#include <dequeue>
+#include <map>
 #include "player.h"
 
 using namespace std;
-const int PlayerCount = 3;
-const int DeckCount = 1;
-const int Rounds = 10;
-
-using suit = array<int,13>;
-suit spades = {1,2,3,4,5,6,7,8,9,10,11,12,13};
-suit clubs = {1,2,3,4,5,6,7,8,9,10,11,12,13};
-suit diamond = {1,2,3,4,5,6,7,8,9,10,11,12,13};
-suit hearts = {1,2,3,4,5,6,7,8,9,10,11,12,13};
-
+using uint = unsigned int;
+class Player;
+enum class PlayerAction: int{
+	No_op =0,
+	Hit = 1, 
+	Stand
+};
          
 class Dealer{
-	Dealer(int deckCount);
-	drawCard();
-	drawDealerHand();
-	drawPlayerHand();
-	shuffleCards();
-private:
-	int cardCount;
-	vector<int> dealerHand;
-	map<int,vector<int>> players;
-	dequeue<int> deck;
-	vector<int> drawnCards;
+public:
+	Dealer();
+	~Dealer();
+	void initializePlayers();
+	uint drawCard();
+	void drawDealerHand();
+	const vector<uint>& showDealerHand();
+	void drawPlayerHands();
+	void setDealerBet(uint);
+	uint getDealerBet() const;
+	void setPlayerBets(uint);
+	void createPlayingDeck();
+	void shuffleDeck();
+	void playGame();
+	void printDeck();
+	void printSortedDeck();
+	void putBackPlayedCards();
+	void actOnPlayerRequest(uint, PlayerAction);
+	void dealerAction(uint);
+	void cleanup();
+	void checkDealerSoftHand(uint, uint);
 
-}
+
+private:
+	vector<uint> mDealerHand;
+	uint mDealerCard;
+	uint mHandValue;
+	//Player index will identify the player,and get access to their info
+	map<uint,Player* > mPlayers;	
+	uint mDealerBet;
+	vector<uint> mDeck;
+	vector<uint> mDrawnCards;
+	bool mSoftHand;
+	static int randomSeed;
+};
 
 
 #endif
--- Blackjack/player.h
+++ Blackjack/player.h
@@ -1,23 +1,46 @@
 #ifndef _PLAYER_H_
 #define _PLAYER_H_
+
 #include <iostream>
 #include <vector>
-#include "dealer.h"
+#include <map>
+#include <array>
 
 using namespace std;
-class Dealer;
+
+enum class action : int{
+	No_op =0,
+	Hit, 
+	Stand
+};
+using actionArray = array<action,12>;
+
 
 class Player{
-	Player(int, Dealer*);
-	void setBet(int);
-	int getBet() const;
-	void getHand();
-	void drawCard();
-	private:
-		int index;
-		Dealer* dealer;
-		vector<int>* cards;
-		vector<int> bet;
+public:
+	Player(uint bet);
+	void setBet(uint);
+	void getHand(uint&, uint&) const;
+	uint getHandValue() const;
+	float getWinPercentage() const;
+	void updateResult(uint);
+	void drawHand(uint, uint);
+	action playHand(uint);
+	action playHitCard(uint card, uint dealerCard);
+	void formulate_strategy();
+	void cleanup();
+	void checkSoftHand(uint,uint);
+	bool isBlackJack();
+private:
+	uint mBet;
+	uint mRoundsPlayed;
+	uint mWins;
+	uint mLosses;
+	vector<uint> mHand;
+	uint mHandValue;
+	bool mSoftHand;
+	map<uint, actionArray> mMap;
+	map<uint, actionArray> mSoftMap;
 };
 
 #endif
